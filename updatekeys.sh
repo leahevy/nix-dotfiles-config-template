7ac0658cb54a0fa80237f42b1dc647c583f4cc43
Leah <leah.nixdev@gmail.com>
2026-04-28 17:06:57 +0200

    Add treefmt config and reformat all

--- updatekeys.sh
+++ updatekeys.sh
@@ -5,12 +5,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 echo "# Checking files at path $PWD"
 find . -type f -print0 | while IFS= read -r -d '' file; do
-  if [[ "$file" != *"/.git/"* ]]; then
-    if sops -d "$file" >/dev/null 2>&1; then
-        echo "++ Updating keys for encrypted file: $file"
-        sops updatekeys --yes "$file"
-    else
-        echo "-- DO not update file: $file"
-    fi
-  fi
+	if [[ "$file" != *"/.git/"* ]]; then
+		if sops -d "$file" >/dev/null 2>&1; then
+			echo "++ Updating keys for encrypted file: $file"
+			sops updatekeys --yes "$file"
+		else
+			echo "-- DO not update file: $file"
+		fi
+	fi
 done
